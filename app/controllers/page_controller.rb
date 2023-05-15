d43a8c37c0727187e2244922dca4b7bbb5c7ce78
gcalquin <gcalquin@gmail.com>
2023-05-15 15:29:49 -0400

    Se agrega name a la vista y con eso se hace el match para que se pueda grabar el registro creado, ademas se agrega una tabla en formato bootstrap

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,10 +1,14 @@
 class PageController < ApplicationController
   def index
+
+     @clientes = Cliente.all
+
   end
 
   def create
-    @cliente = Cliente.new(nombre: params[:nombre], apellido: params[:apellido], edad: params[:edad])
+    @cliente = Cliente.new(nombre: params[:nombrepepito], apellido: params[:apellidopepito], edad: params[:edadpepito])
     @cliente.save
+    ##redirect_to '/pages/index'
   end
 
 end
